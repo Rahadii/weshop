764985c4d629124a7e6f1ee8d0d4c61c84c26a67
Rahadi <rahadirpl@gmail.com>
2019-07-20 10:03:34 +0700

    upload my-profile.php
    for profile page

---
+++ weshop/my-profile.php
@@ -0,0 +1,46 @@
+<?php 
+    if($user_id) {
+        $module = isset($_GET['module']) ? $_GET['module'] : false;
+        $action = isset($_GET['action']) ? $_GET['action'] : false;
+        $mode   = isset($_GET['mode']) ? $_GET['mode'] : false;
+    } else {
+        header("location: ".BASE_URL."index.php?page=login");
+    }
+?>
+
+<div id="bg-page-profile">
+
+    <div id="menu-profile">
+        <ul>
+            <li>
+                <a <?php if($module == "kategori"){ echo "class='active'";} ?> href=<?php echo BASE_URL."index.php?page=my-profile&module=kategori&action=list"; ?>">Kategori</a>
+            </li>
+            <li>
+                <a <?php if($module == "barang"){ echo "class='active'";} ?> href="<?php echo BASE_URL."index.php?page=my-profile&module=barang&action=list"; ?>">Barang</a>
+            </li>
+            <li>
+                <a <?php if($module == "kota"){ echo "class='active'";} ?> href="<?php echo BASE_URL."index.php?page=my-profile&module=kota&action=list"; ?>">Kota</a>
+            </li>
+            <li>
+                <a <?php if($module == "user"){ echo "class='active'";} ?> href="<?php echo BASE_URL."index.php?page=my-profile&module=user&action=list"; ?>">User</a>
+            </li>
+            <li>
+                <a <?php if($module == "banner"){ echo "class='active'";} ?> href="<?php echo BASE_URL."index.php?page=my-profile&module=banner&action=list"; ?>">Banner</a>
+            </li>
+            <li>
+                <a <?php if($module == "pesanan"){ echo "class='active'";} ?> href="<?php echo BASE_URL."index.php?page=my-profile&module=pesanan&action=list"; ?>">Pesanan</a>
+            </li>
+        </ul>
+    </div>
+
+    <div id="profile-content">
+        <?php 
+            $file = "module/$module/$action.php";
+            if (file_exists($file)) {
+                include_once($file);
+            } else {
+                echo "<h3>Sorry, Page Not Found</h3>";
+            }
+        ?>
+    </div>
+</div>
