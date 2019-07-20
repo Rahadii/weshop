4fa59dadb256f2dcffddd428673f3cee7f2f4cbe
Rahadi <rahadirpl@gmail.com>
2019-07-20 11:03:57 +0700

    update

--- weshop/index.php
+++ weshop/index.php
@@ -1,6 +1,12 @@
 <?php
+	session_start();
+
 	include_once("function/helper.php");
 	$page = isset($_GET['page']) ? $_GET['page'] : false;
+
+	$user_id 	= isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
+	$nama 		= isset($_SESSION['nama']) ? $_SESSION['nama'] : false;
+	$level 		= isset($_SESSION['level']) ? $_SESSION['level'] : false;
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,25 +23,34 @@
 
 			<div id="menu">
 				<div id="user">
-					<a href="<?php echo BASE_URL."index.php?page=login"; ?>">Login</a>
-					<a href="<?php echo BASE_URL."index.php?page=register"; ?>">Register</a>
+					<?php
+						if($user_id){
+							echo "Hi, <b>$nama</b>, 
+							<a href='".BASE_URL."index.php?page=my-profile&module=pesanan&action=list'>My Profile</a>
+							<a href='".BASE_URL."logout.php'>Logout</a>";
+						}else{
+							echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
+							<a href='".BASE_URL."index.php?page=register'>Register</a>";
+						}
+					?>
+
 				</div>
 				
 				<a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>" id="button-keranjang">
 					<img src="<?php echo BASE_URL."images/cart.png"; ?>">
 				</a>
+
 			</div>
 		</div>
 
 		<div id="content">
 			<?php
-			// for checking file  
 				$filename = "$page.php";
 				// checking
 				if(file_exists($filename)){
-					include_once($filename); // include if file exist
+					include_once($filename);
 				}else{
-					echo "File Not Found | 404"; // Not Found Page
+					echo "<h3>Sorry, Page Not Found | 404</h3>";
 				}
 			?>
 		</div>
