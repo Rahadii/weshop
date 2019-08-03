eea963f7127efc9bec116436e3dc2f00afa3f785
rahadii <rahadirpl@gmail.com>
2019-08-03 21:39:56 +0700

    Add new file

---
+++ weshop/module/kategori/form.php
@@ -0,0 +1,19 @@
+<form action="<?php echo BASE_URL."module/kategori/action.php"; ?>" method="POST">
+
+<div class="element-form">
+	<label>Kategori</label>
+	<span><input type="text" name="kategori" placeholder="Nama Kategori" autocomplete="off" /></span>
+</div>
+<div class="element-form">
+	<label>Status</label>
+     <span>
+          <input type="radio" name="status" value="on" />On
+          <input type="radio" name="status" value="off" />Off
+     </span>
+</div>
+<div class="element-form">
+	<label>Kategori</label>
+	<span><input type="text" name="kategori" placeholder="Nama Kategori" autocomplete="off" /></span>
+</div>
+
+</form>
