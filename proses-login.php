776ee57b6072655f92603e57193de87dc19e0e91
Rahadii <rahadirpl@gmail.com>
2019-09-14 14:28:51 +0700

    update proses login header:location dan penambahan statement

--- proses-login.php
+++ proses-login.php
@@ -4,10 +4,11 @@
     include_once('function/koneksi.php');
     
     $email      = $_POST['email'];
-    $password   = md5($_POST['password']);
+    $password   = md5($_POST['password']); // hashing password
 
     $query = mysqli_query($db, "SELECT * FROM user WHERE email = '$email' AND password='$password' AND status='on'");
 
+    // cek data
     if (mysqli_num_rows($query) == 0) {
         header("location: ".BASE_URL."index.php?page=login&notif=true");
     }else {
@@ -18,6 +19,10 @@
         $_SESSION['nama']    = $row['nama'];
         $_SESSION['level']   = $row['level'];
 
-        header("location: ".BASE_URL."index.php?page=my-profile&module=pesanan&action=list");
+        if(isset($_SESSION["proses_pesanan"]) && $_SESSION['level'] == "customer"){
+            header("location: ".BASE_URL."index.php?page=data_pemesanan");
+        }else{
+            header("location: ".BASE_URL."index.php?page=my-profile&module=pesanan&action=list");   
+        }
     }
 ?>
